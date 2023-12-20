6ea466412a8a1f1234038c401d1156717659a16e
Raphael Vinicius <raphael.viniciuss@outlook.com>
2023-12-20 10:58:01 -0300

    feat: start new project

---
+++ FlickrAppGarden/FlickrAppGarden/FlickrAppGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrAppGardenApp.swift
+//  FlickrAppGarden
+//
+//  Created by Raphael Vinicius on 20/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FlickrAppGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
